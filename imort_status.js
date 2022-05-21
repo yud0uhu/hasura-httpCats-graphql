3352c35ae1d63c2359833fd4a53e3d435da4d7e9
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-05-22 07:41:46 +0900

    Initial Commit

---
+++ imort_status.js
@@ -0,0 +1,116 @@
+const axios = require('axios');
+const {Client}  = require('graphqurl');
+
+const client = new Client({
+    endpoint: 'http://localhost:8080/v1/graphql',
+    headers: {
+        'Content-Type': 'application/json',
+        'X-Hasura-Access-Key':'mysecretkey',
+    }
+})
+const statuses = [
+    { code: 100, message: 'Continue' },
+    { code: 101, message: 'Switching Protocols' },
+    { code: 102, message: 'Processing' },
+    { code: 200, message: 'OK' },
+    { code: 201, message: 'Created' },
+    { code: 202, message: 'Accepted' },
+    { code: 203, message: 'Non-Authoritative Information' },
+    { code: 204, message: 'No Content' },
+    { code: 206, message: 'Partial Content' },
+    { code: 207, message: 'Multi-Status' },
+    { code: 300, message: 'Multiple Choices' },
+    { code: 301, message: 'Moved Permanently' },
+    { code: 302, message: 'Found' },
+    { code: 303, message: 'See Other' },
+    { code: 304, message: 'Not Modified' },
+    { code: 305, message: 'Use Proxy' },
+    { code: 307, message: 'Temporary Redirect' },
+    { code: 308, message: 'Permanent Redirect' },
+    { code: 400, message: 'Bad Request' },
+    { code: 401, message: 'Unauthorized' },
+    { code: 402, message: 'Payment Required' },
+    { code: 403, message: 'Forbidden' },
+    { code: 404, message: 'Not Found' },
+    { code: 405, message: 'Method Not Allowed' },
+    { code: 406, message: 'Not Acceptable' },
+    { code: 407, message: 'Proxy Authentication Required' },
+    { code: 408, message: 'Request Timeout' },
+    { code: 409, message: 'Conflict' },
+    { code: 410, message: 'Gone' },
+    { code: 411, message: 'Length Required' },
+    { code: 412, message: 'Precondition Failed' },
+    { code: 413, message: 'Payload Too Large' },
+    { code: 414, message: 'Request-URI Too Long' },
+    { code: 415, message: 'Unsupported Media Type' },
+    { code: 416, message: 'Request Range Not Satisfiable' },
+    { code: 417, message: 'Expectation Failed' },
+    { code: 418, message: 'I’m a teapot' },
+    { code: 420, message: 'Enhance Your Calm' },
+    { code: 421, message: 'Misdirected Request' },
+    { code: 422, message: 'Unprocessable Entity' },
+    { code: 423, message: 'Locked' },
+    { code: 424, message: 'Failed Dependency' },
+    { code: 425, message: 'Too Early' },
+    { code: 426, message: 'Upgrade Required' },
+    { code: 429, message: 'Too Many Requests' },
+    { code: 431, message: 'Request Header Fields Too Large' },
+    { code: 444, message: 'No Response' },
+    { code: 450, message: 'Blocked by Windows Parental Controls' },
+    { code: 451, message: 'Unavailable For Legal Reasons' },
+    { code: 497, message: 'HTTP Request Sent to HTTPS Port' },
+    { code: 498, message: 'Token expired/invalid' },
+    { code: 499, message: 'Client Closed Request' },
+    { code: 500, message: 'Internal Server Error' },
+    { code: 501, message: 'Not Implemented' },
+    { code: 502, message: 'Bad Gateway' },
+    { code: 503, message: 'Service Unavailable' },
+    { code: 504, message: 'Gateway Timeout' },
+    { code: 506, message: 'Variant Also Negotiates' },
+    { code: 507, message: 'Insufficient Storage' },
+    { code: 508, message: 'Loop Detected' },
+    { code: 509, message: 'Bandwidth Limit Exceeded' },
+    { code: 510, message: 'Not Extended' },
+    { code: 511, message: 'Network Authentication Required' },
+    { code: 521, message: 'Web Server Is Down' },
+    { code: 522, message: 'Connection Timed Out' },
+    { code: 523, message: 'Origin Is Unreachable' },
+    { code: 525, message: 'SSL Handshake Failed' },
+    { code: 599, message: 'Network Connect Timeout Error' },
+]
+statuses.forEach(async value=>{
+    await axios.get(`https://http.cat/${value.code}.jpg`, {
+    }).then(result => {
+        const createStatus = `mutation insert_status (
+            $status_code: Int,
+            $status_image: String
+            $status_message: String
+        ) {
+            insert_status ( objects: [
+                    {
+                        status_code: $status_code,
+                        status_image: $status_image,
+                        status_message: $status_message,
+                    }
+                ]) {
+                affected_rows
+                    returning {
+                        status_code
+                        status_image
+                        status_message
+                    }
+                }
+              }`
+
+        const statusDatails = {
+            status_code: value.code,
+            status_image: result.data,
+            status_message: value.message
+        }
+
+        console.log(value.message)
+        client.query(createStatus, statusDatails).catch(error =>{
+            console.log(error)
+        })
+    })
+})
